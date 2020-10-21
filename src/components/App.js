6732bd0469264deb4687942d23d7034105b08f77
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-21 20:26:10 +0530

    commit2

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-	disp= false;
-	handleClick= () => {
-		disp= true;
-	}
-    render() {
-    	return(
-    		<div id="main">
-				<button id="click" onClick={this.handleClick}>Click</button>
-				{this.disp? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null}
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      paratext: ""
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      paratext:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    });
+  };
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleClick}>
+          Click
+        </button>
+        <p id="para">{this.paratext}</p>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
